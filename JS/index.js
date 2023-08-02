66e2456c1280ef30f1f33ca51ad2474c1c9619a7
shubhamdalvi <global-config>
2023-08-02 14:17:37 +0530

    initial commit

---
+++ JS/index.js
@@ -0,0 +1,32 @@
+
+
+document.querySelector('.wishes-main').addEventListener('click',function(){
+
+    document.querySelector('.wishes-main').style="visibility: hidden;";
+    document.querySelector('.main1').style="visibility: visible;";
+    document.querySelector('.special').style="visibility: visible;";
+    document.querySelector('#icons').style="visibility: visible;";
+    document.querySelector('#icons1').style="visibility: visible;";
+   
+
+})
+
+// document.querySelector('.main1').addEventListener('click',function(){
+
+//     document.querySelector('.main1').style="visibility: hidden;";
+//     document.querySelector('.wishes-main').style+="visibility: visible;";
+
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+
